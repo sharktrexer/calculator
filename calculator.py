c96854176a1b3a8d22877d1876148b4557b08db0
sharktrexer <sharktrexer@gmail.com>
2022-10-09 13:57:18 -0700

    simple operations

--- calculator.py
+++ calculator.py
@@ -3,3 +3,28 @@
 I, Ibrahim Sydock, pledge  that  I  have  neither  given  nor  received  help  from  anyone  other  than  the 
 instructor/TA for all program components included here!
 """
+
+import math
+
+#operation methods
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+#Dictionary of value to operation function
+OPERATIONS = {
+    '+' : add,
+    '-' : sub,
+    '*' : mul,
+    '/' : div,
+    '^' : math.pow,
+}
+
