6c0be24e0e9316dc658a53007687576534ff4477
sharktrexer <sharktrexer@gmail.com>
2022-10-09 13:00:40 -0700

    This is a new commit for what I originally planned to be amended

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+I, Ibrahim Sydock, pledge  that  I  have  neither  given  nor  received  help  from  anyone  other  than  the 
+instructor/TA for all program components included here!
+"""
+
