631ac9f37b4302126151d27bef989ddce179563f
sharktrexer <sharktrexer@gmail.com>
2022-10-10 12:24:18 -0700

    added all operation methods

--- calculator.py
+++ calculator.py
@@ -4,9 +4,10 @@ I, Ibrahim Sydock, pledge  that  I  have  neither  given  nor  received  help  f
 instructor/TA for all program components included here!
 """
 
+#For sin, log, etc. functions
 import math
 
-#operation methods
+'''operation methods'''
 def add(num1, num2):
     return num1 + num2
 
@@ -19,12 +20,29 @@ def mul(num1, num2):
 def div(num1, num2):
     return num1 / num2
 
-#Dictionary of value to operation function
-OPERATIONS = {
+def cot(num):
+    return math.cos(num) / math.sin(num)
+
+
+"""Dictionaries of value to operation function"""
+# 2 input operations
+DOS_OPERATIONS = {
     '+' : add,
     '-' : sub,
     '*' : mul,
     '/' : div,
-    '^' : math.pow,
+    '^' : math.pow
 }
 
+# 1 input operations
+UNO_OPERATIONS = {
+    'sin' : math.sin,
+    'tan' : math.tan,
+    'cos' : math.cos,
+    'cot' : cot,
+    'log' : math.log10,
+    'ln' : math.log
+    }
+
+expression = input("What would you like to evaluate!?\n")
+print(expression)
